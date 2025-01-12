36964bcf3ba81712413aa3f81c2f0dc4314cae22
Shirolin <shirolin.com@gmail.com>
2025-01-12 20:50:34 +0800

    ✨ feat(模态组件样式): 添加模态组件样式，新增次要按钮样式，优化模态底部按钮布局，更新次要颜色变量

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -33,34 +33,28 @@ const handleCancel = () => {
         <p>{{ modalStore.content }}</p>
       </div>
       <div class="modal-footer">
-        <button v-if="modalStore.showCancel" @click="handleCancel">
-          {{ modalStore.cancelText }}
-        </button>
-        <button @click="handleConfirm">
-          {{ modalStore.confirmText }}
-        </button>
+        <div class="modal-footer-right btn-group">
+          <button class="secondary-btn" v-if="modalStore.showCancel" @click="handleCancel">
+            {{ modalStore.cancelText }}
+          </button>
+          <button class="primary-btn" @click="handleConfirm">
+            {{ modalStore.confirmText }}
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-/* 样式略 */
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, .5);
+<style lang="less" scoped>
+/** */
+.modal-footer-right {
   display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 999;
+  justify-content: flex-end;
 }
 
-.modal {
-  background-color: #fff;
-  padding: 20px;
+.btn-group {
+  display: flex;
+  gap: 10px;
 }
 </style>
