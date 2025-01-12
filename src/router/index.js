5b995b0dc6357147740d22bdaff965bc451cbd0e
Shirolin <shirolin.com@gmail.com>
2025-01-12 18:21:44 +0800

    ✨ feat(路由添加): 新增CourseView和InvoiceView路由，支持课程和账单页面导航

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import ProfileView from '@/views/ProfileView.vue'
+import CourseView from '@/views/CourseView.vue'
+import InvoiceView from '@/views/InvoiceView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +22,21 @@ const router = createRouter({
       path: '/profile',
       name: 'profile',
       component: ProfileView,
+    },
+    {
+      path: '/course',
+      name: 'course',
+      component: CourseView,
+    },
+    {
+      path: '/invoice',
+      name: 'invoice',
+      component: InvoiceView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      redirect: { name: 'home' },
     },
   ],
 })
