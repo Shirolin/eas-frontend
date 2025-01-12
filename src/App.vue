e0e0eab752a2a931a03e13906888416c071a8415
Shirolin <shirolin.com@gmail.com>
2025-01-12 23:19:01 +0800

    ✨ feat(过渡效果优化): 新增过渡包装组件，优化路由视图切换效果

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
+import { useRoute } from 'vue-router';
 import HeaderLayout from './components/layouts/HeaderLayout.vue';
 import ModalComponent from './components/ModalComponent.vue';
 import ScrollToTopButton from './components/ScrollToTopButton.vue';
+import TransitionWrapper from './components/TransitionWrapper.vue';
+
+const route = useRoute();
 </script>
 
 <template>
   <HeaderLayout />
   <main>
-    <RouterView />
+    <TransitionWrapper>
+      <RouterView :key="route.fullPath" />
+    </TransitionWrapper>
     <ModalComponent />
     <ScrollToTopButton />
   </main>
---
+++ src/components/TransitionWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <transition name="fade" mode="out-in">
+    <slot />
+  </transition>
+</template>
+
+<style lang="less" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.25s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
