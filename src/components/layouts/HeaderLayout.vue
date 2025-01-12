51f0ae08eb1f633de5e7f86401ba7414087bd602
Shirolin <shirolin.com@gmail.com>
2025-01-12 16:13:47 +0800

    ✨ feat(头部布局): 添加下拉菜单功能，优化用户信息展示，更新样式以支持下拉效果

--- src/components/layouts/HeaderLayout.vue
+++ src/components/layouts/HeaderLayout.vue
@@ -1,4 +1,28 @@
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const isDropdownOpen = ref(false);
+const dropdownRef = ref(null);
+
+const toggleDropdown = () => {
+  isDropdownOpen.value = !isDropdownOpen.value;
+};
+
+const handleClickOutside = (event) => {
+  if (dropdownRef.value && !dropdownRef.value.contains(event.target)) {
+    isDropdownOpen.value = false;
+  }
+};
+
+let isLogin = true;
+
+onMounted(() => {
+  document.addEventListener('click', handleClickOutside);
+});
+
+onUnmounted(() => {
+  document.removeEventListener('click', handleClickOutside);
+});
 </script>
 <template>
   <header class="header">
@@ -13,10 +37,25 @@
         <li>
           <router-link to="/invoice" class="header-text-btn">账单</router-link>
         </li>
-        <li>
+        <li v-if="!isLogin">
           <router-link to="/login" href="#" class="primary-btn">登录</router-link>
         </li>
       </ul>
+      <div v-if="isLogin" class="nav-item header-dropdown" ref="dropdownRef" @click="toggleDropdown">
+        <span class="header-dropdown-separator">|</span>
+        <a class="nav-link">
+          Shirolin
+        </a>
+        <div class="header-dropdown-content" :class="{ show: isDropdownOpen }">
+          <div class="header-dropdown-content-info-item">
+            <div>shirolin@gmail.com</div>
+            <div>管理员</div>
+          </div>
+          <router-link to="/profile">个人信息</router-link>
+          <router-link to="/setting">设置</router-link>
+          <router-link to="/logout">退出</router-link>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
