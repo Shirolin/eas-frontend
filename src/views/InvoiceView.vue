8cd1c551c8b43809dc5c7f11ad08fd27c48e821d
Shirolin <shirolin.com@gmail.com>
2025-01-12 18:23:48 +0800

    ✨ feat(账单页面): 在InvoiceView中添加账单列表容器；更新样式以支持未来的样式调整

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -2,6 +2,11 @@
 <template>
   <div>
     <div class="page-title">账单</div>
+    <div class="page-container">
+      <!-- 账单列表 -->
+    </div>
   </div>
 </template>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+/** */
+</style>
