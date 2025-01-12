8553cceb8ee3794917dd711d974fc529c51ba465
Shirolin <shirolin.com@gmail.com>
2025-01-12 15:29:40 +0800

    ✨ feat(头部布局): 重构HeaderLayout组件，优化样式和结构，添加新样式和变量以支持头部导航

--- src/components/layouts/HeaderLayout.vue
+++ src/components/layouts/HeaderLayout.vue
@@ -1,75 +1,25 @@
 <script setup>
-console.log('HeaderLayout.vue');
 </script>
 <template>
-  <header>
-    <div class="logo">
+  <header class="header">
+    <div class="header-logo">
       <router-link class="link-no-style" to="/">EAS</router-link>
     </div>
-    <nav>
+    <nav class="header-nav">
       <ul>
-        <li><a href="#">课程</a></li>
-        <li><a href="#">账单</a></li>
         <li>
-          <router-link to="/login" href="#" class="cta-button">登录</router-link>
+          <router-link to="/course" class="header-text-btn">课程</router-link>
+        </li>
+        <li>
+          <router-link to="/invoice" class="header-text-btn">账单</router-link>
+        </li>
+        <li>
+          <router-link to="/login" href="#" class="primary-btn">登录</router-link>
         </li>
       </ul>
     </nav>
   </header>
 </template>
-<style scoped>
-header {
-  background-color: white;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  z-index: 100;
-  padding: 20px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-
-
-.logo {
-  font-weight: bold;
-  font-size: 1.2em;
-}
-
-nav ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  display: flex;
-}
-
-nav li {
-  margin-left: 20px;
-}
-
-nav a {
-  text-decoration: none;
-  color: #333;
-  padding: 8px 12px;
-  border-radius: 4px;
-  transition: background-color 0.3s;
-}
-
-nav a:hover {
-  background-color: #f0f0f0;
-}
-
-.cta-button {
-  background-color: #007bff;
-  color: white;
-  padding: 10px 16px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  text-decoration: none;
-  transition: background-color 0.3s;
-}
-
-.cta-button:hover {
-  background-color: #0056b3;
-}
+<style lang="less" scoped>
+/** */
 </style>
