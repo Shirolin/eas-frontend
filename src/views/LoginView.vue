56bcd51c49e9d4f5e8b9be115fcf1a99164f9753
Shirolin <shirolin.com@gmail.com>
2025-01-12 16:31:56 +0800

    ✨ feat(登录视图): 重构LoginView组件，添加表单验证逻辑，优化错误信息展示，更新样式以支持用户输入

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,23 +1,54 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue';
+
+const username = ref('');
+const password = ref('');
+const errorMessage = ref('');
+
+const handleSubmit = () => {
+  if (!username.value && !password.value) {
+    errorMessage.value = '用户名和密码不能为空';
+    return;
+  }
+
+  if (!username.value) {
+    errorMessage.value = '用户名不能为空';
+    return;
+  }
+
+  if (!password.value) {
+    errorMessage.value = '密码不能为空';
+    return;
+  }
+
+  // ...处理登录逻辑...
+};
+
+// 监听 username 和 password 的变化，当两者中任意一个变化时，清空 errorMessage
+watch([username, password], () => {
+  errorMessage.value = '';
+});
+</script>
 <template>
   <div class="center-content">
     <div class="card login-container">
       <div class="form-title">教务管理系统</div>
       <h2>登录</h2>
-      <form id="loginForm">
+      <form id="loginForm" @submit.prevent="handleSubmit">
         <div class="form-group">
           <label class="form-label" for="username">用户名</label>
-          <input class="form-input" type="username" id="username" name="username" required>
+          <input class="form-input" type="text" id="username" v-model="username" name="username" required>
         </div>
         <div class="form-group">
           <label class="form-label" for="password">密码</label>
-          <input class="form-input" type="password" id="password" name="password" required>
+          <input class="form-input" type="password" id="password" v-model="password" name="password" required>
         </div>
+        <div class="form-error-message">{{ errorMessage }}</div>
         <div class="remember-me">
           <input class="form-checkbox" type="checkbox" id="remember" name="remember">
           <label for="remember" style="margin-bottom: 0;">记住我</label>
         </div>
-        <div class="primary-btn">登录</div>
+        <div class="primary-btn" @click="handleSubmit">登录</div>
       </form>
     </div>
   </div>
