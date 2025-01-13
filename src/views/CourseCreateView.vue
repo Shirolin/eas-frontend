96db9984d2ecc0dd0de8b6a2c34069f2bfa18cd7
Shirolin <shirolin.com@gmail.com>
2025-01-13 15:04:45 +0800

    ✨ feat(CourseCreateView): 重构课程创建视图，更新表单字段和样式，优化课程创建逻辑

--- src/views/CourseCreateView.vue
+++ src/views/CourseCreateView.vue
@@ -1,98 +1,69 @@
-<template>
-  <div class="course-create">
-    <h2>创建课程</h2>
-    <form @submit.prevent="createCourse">
-      <div class="form-group">
-        <label for="course-name">课程名称</label>
-        <input type="text" id="course-name" v-model="courseName" required />
-      </div>
-      <div class="form-group">
-        <label for="course-description">课程描述</label>
-        <textarea id="course-description" v-model="courseDescription" required></textarea>
-      </div>
-      <div class="form-group">
-        <label for="course-fee">课程费用</label>
-        <input type="number" id="course-fee" v-model="courseFee" required />
-      </div>
-      <button type="submit" class="primary-btn">创建</button>
-      <router-link to="/course" class="secondary-btn">返回</router-link>
-    </form>
-  </div>
-</template>
-
+<!-- CREATE TABLE "public"."courses" (
+  "id" int8 NOT NULL DEFAULT nextval('courses_id_seq'::regclass),
+  "teacher_id" int4 NOT NULL,
+  "teacher_nickname" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "unit_fee" numeric(10,2) NOT NULL,
+  "status" int2 NOT NULL,
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0),
+  CONSTRAINT "courses_pkey" PRIMARY KEY ("id"),
+  CONSTRAINT "courses_teacher_id_name_unique" UNIQUE ("teacher_id", "name")
+); -->
 <script setup>
 import { ref } from 'vue';
+import { useUserStore } from '@/stores/user';
 import { useCourseStore } from '@/stores/course';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
+const userStore = useUserStore();
 const courseStore = useCourseStore();
 const courseName = ref('');
-const courseDescription = ref('');
-const courseFee = ref(0);
+const courseUnitFee = ref(0);
+const courseTeacher = ref(userStore.userData.nickname);
+const courseTeacherId = ref(userStore.userData.id);
 
 const createCourse = async () => {
-  await courseStore.createCourse({
-    name: courseName.value,
-    description: courseDescription.value,
-    fee: courseFee.value,
+  let params = {
+    course: {
+      name: courseName.value,
+      unit_fee: courseUnitFee.value,
+      teacher_id: courseTeacherId.value,
+    },
+    sub_courses: [],
+  }
+  console.log(params);
+  await courseStore.createCourse(params).then(() => {
+    router.push('/course');
   });
   // 跳转到课程列表页面
 };
 </script>
 
-<style scoped>
-.course-create {
-  max-width: 40rem;
-  margin: 2rem auto;
-  padding: 1rem;
-  background-color: #fff;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-  border-radius: 0.5rem;
-}
-
-.form-group {
-  margin-bottom: 1rem;
-}
-
-.form-group label {
-  display: block;
-  margin-bottom: 0.5rem;
-  font-weight: bold;
-}
-
-.form-group input,
-.form-group textarea {
-  width: 100%;
-  padding: 0.5rem;
-  border: 1px solid #ddd;
-  border-radius: 0.25rem;
-}
-
-.primary-btn {
-  padding: 0.5rem 1rem;
-  border: none;
-  border-radius: 0.25rem;
-  background-color: #007bff;
-  color: #fff;
-  cursor: pointer;
-  transition: background-color 0.3s;
-}
-
-.primary-btn:hover {
-  background-color: #0056b3;
-}
-
-.secondary-btn {
-  padding: 0.5rem 1rem;
-  border: none;
-  border-radius: 0.25rem;
-  background-color: #6c757d;
-  color: #fff;
-  cursor: pointer;
-  transition: background-color 0.3s;
-  margin-left: 0.5rem;
-}
-
-.secondary-btn:hover {
-  background-color: #5a6268;
-}
+<template>
+  <div class="list-detail">
+    <h2>创建课程</h2>
+    <form @submit.prevent="createCourse">
+      <div class="form-group">
+        <label class="form-label" for="course-name">课程名称</label>
+        <input class="form-input" type="text" id="course-name" v-model="courseName" required />
+      </div>
+      <div class="form-group">
+        <label class="form-label" for="course-unit-fee">课程单价</label>
+        <input class="form-input" type="number" id="course-unit-fee" v-model="courseUnitFee" required />
+      </div>
+      <div class="form-group">
+        <label class="form-label" for="course-treacher">教师</label>
+        <input class="form-input" type="text" id="course-treacher" v-model="courseTeacher" disabled />
+      </div>
+      <div class="btn-group">
+        <router-link to="/course" class="btn-group-item secondary-btn">返回</router-link>
+        <button type="submit" class="btn-group-item primary-btn">创建</button>
+      </div>
+    </form>
+  </div>
+</template>
+<style lang="less" scoped>
+/** */
 </style>
