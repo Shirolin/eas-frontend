84dd4f9defa3c3f80ae300128a39e7925b3365a2
Shirolin <shirolin.com@gmail.com>
2025-01-13 14:40:11 +0800

    ✨ feat(CourseDetailView): 添加加载指示器，优化课程详情加载体验，使用防抖功能提升性能

--- src/views/CourseDetailView.vue
+++ src/views/CourseDetailView.vue
@@ -3,6 +3,8 @@ import { ref, onMounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { useCourseStore } from '@/stores/course';
 import { useUserStore } from '@/stores/user';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
+import { useDebounce } from '@/utils/useDebounce';
 
 const route = useRoute();
 const router = useRouter();
@@ -10,15 +12,16 @@ const courseStore = useCourseStore();
 const userStore = useUserStore();
 const isTeacher = userStore.isTeacher();
 const course = ref({});
+const loading = ref(false);
 
 const fetchCourseDetail = async () => {
+  loading.value = true;
   const courseId = route.params.id;
   course.value = await courseStore.fetchCourse(courseId);
+  loading.value = false;
 };
 
-const editCourse = () => {
-  // 跳转到课程编辑页面
-};
+const debouncedFetchCourseDetail = useDebounce(fetchCourseDetail, 300);
 
 const deleteCourse = async () => {
   const courseId = route.params.id;
@@ -27,24 +30,29 @@ const deleteCourse = async () => {
 };
 
 onMounted(() => {
-  fetchCourseDetail();
+  debouncedFetchCourseDetail();
 });
 </script>
 <template>
   <div class="list-detail">
-    <h2>{{ course.name }}</h2>
-    <p>{{ course.description }}</p>
-    <div class="list-details">
-      <span>教师: {{ course.teacher_nickname }}</span>
-      <span>子课程数: {{ course.sub_courses_count }}</span>
-      <span>学生数: {{ course.students_count }}</span>
-      <span>单价: {{ course.unit_fee }}</span>
+    <div v-if="loading" class="loading">
+      <LoadingSpinner />
     </div>
-    <div class="list-detail-actions" v-if="isTeacher">
-      <div class="btn-group">
-        <router-link to="/course" class="btn-group-item secondary-btn">返回</router-link>
-        <router-link :to="`/course/${course.id}/edit`" class="btn-group-item primary-btn">编辑</router-link>
-        <div class="btn-group-item red-btn" @click="deleteCourse">删除</div>
+    <div v-else>
+      <h2>{{ course.name }}</h2>
+      <p>{{ course.description }}</p>
+      <div class="list-details">
+        <span>教师: {{ course.teacher_nickname }}</span>
+        <span>子课程数: {{ course.sub_courses_count }}</span>
+        <span>学生数: {{ course.students_count }}</span>
+        <span>单价: {{ course.unit_fee }}</span>
+      </div>
+      <div class="list-detail-actions" v-if="isTeacher">
+        <div class="btn-group">
+          <router-link to="/course" class="btn-group-item secondary-btn">返回</router-link>
+          <router-link :to="`/course/${course.id}/edit`" class="btn-group-item primary-btn">编辑</router-link>
+          <div class="btn-group-item red-btn" @click="deleteCourse">删除</div>
+        </div>
       </div>
     </div>
   </div>
