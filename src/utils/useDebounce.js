3ce72c5949a7cbd92358bf1f344f9568c01017d8
Shirolin <shirolin.com@gmail.com>
2025-01-13 10:49:58 +0800

    ✨ feat(useDebounce.js): 新增立即执行选项以增强防抖函数的灵活性

--- src/utils/useDebounce.js
+++ src/utils/useDebounce.js
@@ -1,14 +1,19 @@
 import { ref, onUnmounted } from 'vue'
 
-export function useDebounce(func, delay = 300) {
+export function useDebounce(func, delay = 300, immediate = false) {
   const debouncedFunc = ref(null)
 
   const debounced = (...args) => {
     if (debouncedFunc.value) {
       clearTimeout(debouncedFunc.value)
     }
-    debouncedFunc.value = setTimeout(() => {
+    if (immediate && !debouncedFunc.value) {
       func.apply(this, args)
+    }
+    debouncedFunc.value = setTimeout(() => {
+      if (!immediate) {
+        func.apply(this, args)
+      }
       debouncedFunc.value = null
     }, delay)
   }
