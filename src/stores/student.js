8920097a2940af71bcf5dcd702bdf3470d89ef07
Shirolin <shirolin.com@gmail.com>
2025-01-13 17:31:44 +0800

    ✨ feat(student store): 增加每页学生数量限制至100，优化学生列表分页体验

--- src/stores/student.js
+++ src/stores/student.js
@@ -6,7 +6,7 @@ export const useStudentStore = defineStore(
   'student',
   () => {
     const DEFAULT_PAGE = 1
-    const DEFAULT_PER_PAGE = 10
+    const DEFAULT_PER_PAGE = 100
 
     const students = ref([])
     const currentPage = ref(DEFAULT_PAGE)
