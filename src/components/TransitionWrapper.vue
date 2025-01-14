d3dfe154b007ce0a9317f8637b6ae6e1edcd3048
Shirolin <shirolin.com@gmail.com>
2025-01-14 13:48:27 +0800

    ✨ feat(TransitionWrapper): 使用动态组件渲染路由匹配的组件，优化过渡效果

--- src/components/TransitionWrapper.vue
+++ src/components/TransitionWrapper.vue
@@ -1,16 +1,25 @@
+<script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const Component = computed(() => route.matched[0]?.components.default);
+
+</script>
 <template>
   <transition name="fade" mode="out-in">
-    <slot />
+    <component :is="Component">
+      <slot />
+    </component>
   </transition>
 </template>
-
 <style lang="less" scoped>
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.25s;
 }
 
-.fade-enter,
+.fade-enter-from,
 .fade-leave-to {
   opacity: 0;
 }
