0b772b0804402f13a513bab5d1339f5acb57d4c2
Shirolin <shirolin.com@gmail.com>
2025-01-14 13:42:45 +0800

    ✨ feat(App): 移动 ScrollToTopButton 组件到公共目录，优化组件结构

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { useRoute } from 'vue-router';
 import HeaderLayout from './components/layouts/HeaderLayout.vue';
 import ModalComponent from './components/common/ModalComponent.vue';
-import ScrollToTopButton from './components/ScrollToTopButton.vue';
+import ScrollToTopButton from './components/common/ScrollToTopButton.vue';
 import TransitionWrapper from './components/TransitionWrapper.vue';
 
 const route = useRoute();
