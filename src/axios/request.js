da44430bd8e77ddb0f97009d343b22d3cac96d61
Shirolin <shirolin.com@gmail.com>
2025-01-14 11:06:26 +0800

    ✨ feat(axios/request): 优化响应拦截器，简化错误处理逻辑，增强未授权处理

--- src/axios/request.js
+++ src/axios/request.js
@@ -3,7 +3,7 @@ import router from '@/router/index.js'
 import { useUserStore } from '@/stores/user'
 import { useToast } from '@/utils/useToast'
 
-const { showToast } = useToast() // 使用 useToast
+const { showToast } = useToast()
 
 // 创建axios实例
 const request = axios.create({
@@ -32,15 +32,13 @@ request.interceptors.request.use(
 // 响应拦截器
 request.interceptors.response.use(
   (response) => {
-    if (response.status === 200) {
-      return response.data
-    } else {
-      return Promise.reject(response) // 返回错误响应
-    }
+    return response.data
   },
   (error) => {
     if (error.response) {
       handleErrorResponse(error.response)
+    } else {
+      showToast('网络错误，请稍后重试', 'error')
     }
     return Promise.reject(error)
   },
@@ -53,12 +51,7 @@ request.interceptors.response.use(
 function handleErrorResponse(response) {
   switch (response.status) {
     case 401:
-      if (response.data.data.isLogin == true) {
-        showToast(response.data.message, 'error')
-      } else {
-        showToast('未授权', 'error')
-        handleUnauthorized()
-      }
+      handleUnauthorized(response)
       break
     case 404:
       showToast('请求资源不存在', 'error')
@@ -70,10 +63,6 @@ function handleErrorResponse(response) {
       showToast('服务器异常', 'error')
       break
     default:
-      console.error({
-        msg: '[响应拦截器]服务器异常',
-        error: response,
-      })
       showToast('服务器异常', 'error')
   }
 }
@@ -86,19 +75,23 @@ function handleValidationError(response) {
     let msg = Object.values(response.data.message).join('，')
     showToast(msg, 'error')
   } catch (e) {
-    throw new Error(e)
+    console.error('Validation error:', e)
   }
 }
 
 /**
  * 处理未授权
  */
-function handleUnauthorized() {
-  console.log('handleUnauthorized')
-  if (!store) store = useUserStore()
-  store.token = null
-  store.userData = {}
-  router.push('/login')
+function handleUnauthorized(response) {
+  if (response.data.data.isLogin) {
+    showToast(response.data.message, 'error')
+  } else {
+    showToast('未授权', 'error')
+    if (!store) store = useUserStore()
+    store.token = null
+    store.userData = {}
+    router.push('/login')
+  }
 }
 
 export default request
